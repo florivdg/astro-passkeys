f5ecf55ead5d9c6da445e3d7fe42f581428e3bd6
Florian van der Galiën <hallo@flori.dev>
2025-03-26 14:17:28 +0100

    Add Node adapter and update dependencies for improved server support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
+
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   integrations: [vue()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+})
